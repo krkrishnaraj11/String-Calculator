9a5207e1472358d61212edce3f3371156cb6f738
Krishnaraj Rajendran <krkrishnaraj11@gmail.com>
2025-01-31 09:33:02 +0530

    Test and add function intial Implementation

--- stringCalculator.js
+++ stringCalculator.js
@@ -2,3 +2,5 @@ function add(numbers) {
     if (!numbers) return 0;
 
 }
+
+module.exports = add;
---
+++ stringCalculator.test.js
@@ -0,0 +1,9 @@
+// Jest Test Cases
+
+const add = require('./stringCalculator');
+
+describe('String Calculator', () => {
+    test('returns 0 for an empty string', () => {
+        expect(add('')).toBe(0);
+    })
+});
